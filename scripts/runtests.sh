2fec94ad1a0aa9eda179347699c11a5684ea89bb
Bidnij <bidnaymina@gmail.com>
2020-12-03 11:17:23 +0200

    Update runtests.sh

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,8 +1,7 @@
 ##!/bin/sh -xe
 
 
-git clone https://bidnaymina@gmail.com:gamer31051995
-@github.com/Bidnij/ComplyCloudAutomationTest
+git clone https://bidnaymina@gmail.com:gamer31051995@github.com/Bidnij/ComplyCloudAutomationTest
 
 cd TeamDenmark.AthleteApp.UIAutomationTest
 
