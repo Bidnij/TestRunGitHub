6a02e50300cd5b1028373da6d9de3d9beb6d144c
Bidnij <bidnaymina@gmail.com>
2020-12-03 10:38:50 +0200

    Create runtests.sh

---
+++ scripts/runtests.sh
@@ -0,0 +1,13 @@
+##!/bin/sh -xe
+
+
+git clone https://bidnaymina@gmail.com:gamer31051995
+@github.com/Bidnij/ComplyCloudAutomationTest
+
+cd TeamDenmark.AthleteApp.UIAutomationTest
+
+ls -la
+
+./gradlew clean test
+cd build
+ls -la
